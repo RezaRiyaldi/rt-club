afd3e71a58a134aceb1ee2739d42b26eb869af99
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2025-10-03 00:59:15 +0700

    Add Config Auth

---
+++ app/Config/Auth.php
@@ -0,0 +1,14 @@
+<?php
+
+use Myth\Auth\Config\Auth as ConfigAuth;
+
+class Auth extends ConfigAuth {
+    public $views = [
+        'login'           => 'auth\login',
+        'register'        => 'auth\register',
+        'forgot'          => 'Myth\Auth\Views\forgot',
+        'reset'           => 'Myth\Auth\Views\reset',
+        'emailForgot'     => 'Myth\Auth\Views\emails\forgot',
+        'emailActivation' => 'Myth\Auth\Views\emails\activation',
+    ];
+}
