3d50b16ac3c8d71523b6e18f5157a9e2b662f71d
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2025-10-03 02:19:52 +0700

    fix route auth view

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -4,11 +4,11 @@ use Myth\Auth\Config\Auth as ConfigAuth;
 
 class Auth extends ConfigAuth {
     public $views = [
-        'login'           => 'auth\login',
-        'register'        => 'auth\register',
-        'forgot'          => 'Myth\Auth\Views\forgot',
-        'reset'           => 'Myth\Auth\Views\reset',
-        'emailForgot'     => 'Myth\Auth\Views\emails\forgot',
-        'emailActivation' => 'Myth\Auth\Views\emails\activation',
+        'login'           => 'auth/login',
+        'register'        => 'auth/register',
+        'forgot'          => 'Myth/Auth/Views/forgot',
+        'reset'           => 'Myth/Auth/Views/reset',
+        'emailForgot'     => 'Myth/Auth/Views/emails/forgot',
+        'emailActivation' => 'Myth/Auth/Views/emails/activation',
     ];
 }
